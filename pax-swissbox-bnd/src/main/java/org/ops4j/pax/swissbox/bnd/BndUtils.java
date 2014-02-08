126cd1cce31651923301a46deb2da86defff7793
Freeman Fang <freeman.fang@gmail.com>
2014-02-08 12:17:20 +0800

    [PAXSB-75]Avoid 'Bundle could not be generated' WARN message when InputStream is being partially read (cherry picked from commit e33c270699bc6b9014bffaa09c1ed8a1e7677045)

--- pax-swissbox-bnd/src/main/java/org/ops4j/pax/swissbox/bnd/BndUtils.java
+++ pax-swissbox-bnd/src/main/java/org/ops4j/pax/swissbox/bnd/BndUtils.java
@@ -182,7 +182,7 @@ public class BndUtils
     private static PipedInputStream createInputStream( final Jar jar )
         throws IOException
     {
-        final PipedInputStream pin = new PipedInputStream();
+        final CloseAwarePipedInputStream pin = new CloseAwarePipedInputStream();
         final PipedOutputStream pout = new PipedOutputStream( pin );
 
         new Thread()
@@ -195,7 +195,15 @@ public class BndUtils
                 }
                 catch( Exception e )
                 {
-                    LOG.warn( "Bundle cannot be generated", e );
+                    if (pin.closed)
+                    {
+                        // logging the message at DEBUG logging instead
+                        // -- reading thread probably stopped reading
+                        LOG.debug( "Bundle cannot be generated" );
+                    }
+                    else {
+                        LOG.warn( "Bundle cannot be generated", e );
+                    }
                 }
                 finally
                 {
@@ -343,4 +351,17 @@ public class BndUtils
         throw exception;
     }
 
+    /**
+     * PipedInputStream implementation that keeps track of whether it has been closed or not.
+     */
+    private static final class CloseAwarePipedInputStream extends PipedInputStream
+    {
+        private boolean closed = false;
+
+        public void close() throws IOException
+        {
+            closed = true;
+            super.close();
+        }
+    }
 }
